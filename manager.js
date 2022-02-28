dfe8930de6b47cee5af952facc8cc8ca70f45780
Yaroslav Badyora <y.badyora@fulcrum.rocks>
2022-02-28 02:00:28 +0200

    update

--- manager.js
+++ manager.js
@@ -12,13 +12,13 @@ const logger = pino(transport)
 const endpoints = targets.map((params) => new Endpoint(params, logger))
 
 const EXECUTION_LIMIT = 100000
-const TIMEOUT_BASE = 1000
+const TIMEOUT_BASE = 2000
 
 const run = async () => {
   for (let i = 0; i < EXECUTION_LIMIT; i++) {
     await sleepRand(TIMEOUT_BASE, TIMEOUT_BASE + 1000)
     endpoints.forEach(async endpoint => {
-      await sleep(500)
+      await sleep(300)
       if (endpoint.stats.errorsInRow > 5)
         endpoint.cooldown(60 * 2) // cooldown for 2 min
       else
