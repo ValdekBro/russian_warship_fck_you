2a12d68e67f9ba25fe3ad824fef7bbb200f90195
Yaroslav Badyora <y.badyora@fulcrum.rocks>
2022-03-01 03:23:32 +0200

    update

--- ip-changer.js
+++ ip-changer.js
@@ -6,7 +6,7 @@ const REGION = 'asia-east2-a'
 let accessToken = `ya29.c.b0AXv0zTPVk4Z4b1Vif7CZYy1Pi7H9dU9p5aLjJ8CVUR5Zg0C2fDEEEmIqP0mBq92R4BjmLnYWFPNM2YDkf2aaSLe5obx6WrRB6r9VANmfCBcLajUpG5hAgou0vNfCHt8Xp9HFz3SCaeSbF1AxPKtJIugtoRf8ucxLI0VOu1MedlZcfSZp3LGvTye_DbdDK0KXjhbkQehWDqrhZPl7yXVSWdk`
 
 const updateAccessToken = async () => {
-    const response = await axios.get('https://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token', { headers: {
+    const response = await axios.get("http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token", { headers: {
         "Metadata-Flavor": "Google"
     }})
 
